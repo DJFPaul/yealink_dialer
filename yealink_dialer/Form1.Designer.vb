2a55818f633589a4fd624b52d05c2ce8316214e8
FPaul <>
2023-12-26 16:02:42 +0100

    Initial Commit

---
+++ yealink_dialer/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CloseTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.CallDelay = New System.Windows.Forms.Timer(Me.components)
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Closer = New System.Windows.Forms.Timer(Me.components)
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(0, -1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(341, 34)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "No number provided"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.WebBrowser1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 106)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(273, 168)
+        Me.Panel1.TabIndex = 4
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(273, 168)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Label2.Location = New System.Drawing.Point(16, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(150, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "no account"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(246, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 32)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Call"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CloseTimer
+        '
+        Me.CloseTimer.Interval = 1000
+        '
+        'CallDelay
+        '
+        Me.CallDelay.Interval = 3000
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(5, 6)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox1.Size = New System.Drawing.Size(238, 58)
+        Me.RichTextBox1.TabIndex = 9
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(246, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 24)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Test"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Closer
+        '
+        Me.Closer.Interval = 5000
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(12, 118)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox2.Size = New System.Drawing.Size(181, 58)
+        Me.RichTextBox2.TabIndex = 11
+        Me.RichTextBox2.Text = ""
+        Me.RichTextBox2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 69)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = " Yealink Dialer"
+        Me.TopMost = True
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CloseTimer As Timer
+    Friend WithEvents CallDelay As Timer
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Closer As Timer
+    Friend WithEvents RichTextBox2 As RichTextBox
+End Class
---
+++ yealink_dialer/Form1.vb
@@ -0,0 +1,128 @@
+﻿Imports System.Security.Cryptography
+Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+    Dim DIAL As String
+    Dim PHONEIP As String
+    Dim SIPACCOUNT As String
+    Dim USERNAME As String
+    Dim PASSWORD As String
+    Dim CTime As Integer = 8
+    Dim CRDTA As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ConfigContent As String = My.Computer.FileSystem.ReadAllText(Application.StartupPath() & "\data\config.ini")
+        RichTextBox2.Text = ConfigContent
+        CTime = RichTextBox2.Lines(1).Replace("closedelay=", "") - 1
+        CallDelay.Interval = RichTextBox2.Lines(3).Replace("dialdelay=", "") & "000"
+
+        Dim PhoneContent As String = My.Computer.FileSystem.ReadAllText(Application.StartupPath() & "\data\phone.cfg")
+
+        Dim rd As New RijndaelManaged
+        Dim rijndaelIvLength As Integer = 16
+        Dim md5 As New MD5CryptoServiceProvider
+        Dim key() As Byte = md5.ComputeHash(Encoding.UTF8.GetBytes("duSK(57&5fHPskg8%$/3jalskg90DGNA51"))
+        md5.Clear()
+        Dim encdata() As Byte = Convert.FromBase64String(PhoneContent)
+        Dim ms As New MemoryStream(encdata)
+        Dim iv(15) As Byte
+        ms.Read(iv, 0, rijndaelIvLength)
+        rd.IV = iv
+        rd.Key = key
+        Dim cs As New CryptoStream(ms, rd.CreateDecryptor, CryptoStreamMode.Read)
+        Dim data(ms.Length - rijndaelIvLength) As Byte
+        Dim i As Integer = cs.Read(data, 0, data.Length)
+        CRDTA = System.Text.Encoding.UTF8.GetString(data, 0, i)
+        cs.Close()
+        rd.Clear()
+
+        RichTextBox1.Text = CRDTA
+
+        PHONEIP = RichTextBox1.Lines(0).Replace("PHONEIP=", "")
+        SIPACCOUNT = RichTextBox1.Lines(1).Replace("SIPACCOUNT=", "")
+        USERNAME = RichTextBox1.Lines(2).Replace("USERNAME=", "")
+        PASSWORD = RichTextBox1.Lines(3).Replace("PASSWORD=", "")
+
+        Try
+            DIAL = My.Application.CommandLineArgs(0).Replace("tel:", "")
+            DIAL = DIAL.Replace("/", "")
+            DIAL = DIAL.Replace("-", "")
+            Label1.Text = "Calling: " & DIAL
+            If RichTextBox2.Lines(0).Replace("autoclose=", "") = "true" Then CloseTimer.Start()
+            If RichTextBox2.Lines(2).Replace("autodial=", "") = "true" Then CallDelay.Start()
+        Catch
+            Label1.Text = ""
+            Button2.Show()
+            Button1.Text = "Save"
+            RichTextBox1.Show()
+            DIAL = "012345678910"
+        Finally
+            Label2.Text = "Phone IP: " & PHONEIP
+        End Try
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "Save" Then
+
+            Dim rd As New RijndaelManaged
+            Dim md5 As New MD5CryptoServiceProvider
+            Dim key() As Byte = md5.ComputeHash(Encoding.UTF8.GetBytes("duSK(57&5fHPskg8%$/3jalskg90DGNA51"))
+            md5.Clear()
+            rd.Key = key
+            rd.GenerateIV()
+            Dim iv() As Byte = rd.IV
+            Dim ms As New MemoryStream
+            ms.Write(iv, 0, iv.Length)
+            Dim cs As New CryptoStream(ms, rd.CreateEncryptor, CryptoStreamMode.Write)
+            Dim data() As Byte = System.Text.Encoding.UTF8.GetBytes(RichTextBox1.Text)
+            cs.Write(data, 0, data.Length)
+            cs.FlushFinalBlock()
+            Dim encdata() As Byte = ms.ToArray()
+            CRDTA = Convert.ToBase64String(encdata)
+            cs.Close()
+            rd.Clear()
+
+            Using writer As New System.IO.StreamWriter(Application.StartupPath() & "\data\phone.cfg")
+                For Each line In "A"
+                    writer.Write(CRDTA)
+                Next
+            End Using
+        Else
+            CloseTimer.Stop()
+            WebBrowser1.Navigate("http://" & USERNAME & ":" & PASSWORD & "@" & PHONEIP & "/servlet?key=number=" & DIAL & "&outgoing_uri=" & SIPACCOUNT & "")
+            Closer.Start()
+            Me.Hide()
+
+        End If
+    End Sub
+
+    Private Sub CloseTimer_Tick(sender As Object, e As EventArgs) Handles CloseTimer.Tick
+        If CTime > 0 Then
+            CTime = CTime - 1
+        Else
+            Close()
+        End If
+    End Sub
+
+    Private Sub CallDelay_Tick(sender As Object, e As EventArgs) Handles CallDelay.Tick
+        WebBrowser1.Navigate("http://" & USERNAME & ":" & PASSWORD & "@" & PHONEIP & "/servlet?key=number=" & DIAL & "&outgoing_uri=" & SIPACCOUNT & "")
+        CallDelay.Stop()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Initiate test call?" & vbNewLine & "(Calls 01234567890)", MsgBoxStyle.YesNo)
+        PHONEIP = RichTextBox1.Lines(0).Replace("PHONEIP=", "")
+        SIPACCOUNT = RichTextBox1.Lines(1).Replace("SIPACCOUNT=", "")
+        USERNAME = RichTextBox1.Lines(2).Replace("USERNAME=", "")
+        PASSWORD = RichTextBox1.Lines(3).Replace("PASSWORD=", "")
+
+        WebBrowser1.Navigate("http://" & USERNAME & ":" & PASSWORD & "@" & PHONEIP & "/servlet?key=number=" & DIAL & "&outgoing_uri=" & SIPACCOUNT & "")
+    End Sub
+
+    Private Sub Closer_Tick(sender As Object, e As EventArgs) Handles Closer.Tick
+        Close()
+    End Sub
+
+End Class
